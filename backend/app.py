bfd2a966a116b104e220b4652b7800c18fadd222
TrisNol <tristan.nolde@yahoo.de>
2022-04-19 14:53:49 +0200

    migration to FastAPI

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS, cross_origin
+import json
 import logging
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 import utils.ml_utils as util
 
@@ -8,37 +10,41 @@ model = util.getModel(path="./model/")
 encoder = util.getEncoder(path="./encoder/classes.npy")
 
 
-app = Flask(__name__)
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 logging.basicConfig(filename='demo.log', level=logging.DEBUG)
-cors = CORS(app)
 
-# @app.route('/<path:path>', methods=['GET'])
-# def static_proxy(path):
-#     if path.endswith(".js"): 
-#         return send_from_directory('./static', path, mimetype="application/javascript")
-#     return send_from_directory('./static', path)
+from pydantic import BaseModel
+class ImageModel(BaseModel):
+    image: str
 
+class Categories(BaseModel):
+    categories: list
 
-# @app.route('/')
-# def root():
-#   return send_from_directory('./static', 'index.html')
-
-
-@app.route('/categories', methods=['POST'])
-@cross_origin()
-def process_image_external():
+@app.post('/categories')
+def process_image_external(image: ImageModel):
     try:
         # get base64 encoded image transformed
-        image = util.decode_img(request.json['image'])
+        image = util.decode_img(image.image)
 
         predictions = model.predict(image)
         predictions = util.transform_predictions(predictions)
         predictions = encoder.inverse_transform(predictions)
 
-        return jsonify({'categories': predictions[0]})
+        return Categories(categories=predictions[0])
     except Exception as e:
         print(e)
-        return "Bad Request", 400
+        raise HTTPException(400)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
